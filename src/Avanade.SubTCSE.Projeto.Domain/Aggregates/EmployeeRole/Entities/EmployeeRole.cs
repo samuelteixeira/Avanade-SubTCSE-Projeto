2abf446fa10e2ba6a6972f6ae668e8bd32521954
Samuel Teixeira <samuelteixeira.dev@gmail.com>
2021-06-15 09:23:53 -0300

    arquitetura DDD

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; }
+
+        public string RoleName { get; set; }
+        
+    }
+}
